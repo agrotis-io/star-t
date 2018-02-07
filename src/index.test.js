4c4a1083ecf66177605f5ba2926bf615630857ba
João Henrique Serodio <joao.serodio@agrotis.com>
2018-02-07 14:48:36 -0200

    style - atualiza code format de index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,8 @@
-import sayHello from '../src'
+import sayHello from '../src';
 
 describe('sayHello', () => {
   it('returns hello', () => {
-    expect(sayHello()).toBe('getting Start-t, agrotis!')
-    expect(sayHello('world')).toBe('getting Start-t, world!')
-  })
-})
+    expect(sayHello()).toBe('getting Start-t, agrotis!');
+    expect(sayHello('world')).toBe('getting Start-t, world!');
+  });
+});
